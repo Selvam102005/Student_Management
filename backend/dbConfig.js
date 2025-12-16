b5d207151cb7551522d57c70fccd43dfa5d2d4d1
Selvam102005 <selvam.s102005@gmail.com>
2025-12-16 19:24:02 +0530

    Update dbConfig.js

--- backend/dbConfig.js
+++ backend/dbConfig.js
@@ -1,8 +1,3 @@
 export const dbConfig = {
-  user: "CloudSA175729ed",
-  password: "Mallika@102005", 
-  database: "student",
-  server: "selvam.database.windows.net",
-  pool: { max: 10, min: 0, idleTimeoutMillis: 30000 },
-  options: { encrypt: true, trustServerCertificate: false },
+  
 };
